a04d6a973fb60602876cf66d701567c4463b20ef
Georgi Kirilov <gkkirilov@gmail.com>
2026-01-12 01:08:32 +0200

    Rename and refactor mod to create "Audio Output Device Switcher"
    This commit updates the mod to allow users to switch audio output devices using Ctrl+Scroll over the volume icon. The mod's name, description, and settings have been modified accordingly. New functionality includes device enumeration, default device switching, and user notifications. The previous implementation for Microsoft Paint has been removed, and the codebase has been significantly expanded to support the new features.

--- mod.wh.cpp
+++ mod.wh.cpp
@@ -1,143 +1,393 @@
 // ==WindhawkMod==
-// @id              new-mod
-// @name            Your Awesome Mod
-// @description     The best mod ever that does great things
-// @version         0.1
+// @id              audio-scroll-switcher
+// @name            Audio Output Device Switcher
+// @description     Ctrl+Scroll on the volume icon to switch audio devices
+// @version         1.0
 // @author          You
-// @github          https://github.com/nat
-// @twitter         https://twitter.com/jack
-// @homepage        https://your-personal-homepage.example.com/
-// @include         mspaint.exe
-// @compilerOptions -lcomdlg32
+// @github          https://github.com/you
+// @include         explorer.exe
+// @compilerOptions -lole32 -loleaut32 -lpropsys -lgdi32 -luser32
 // @license         MIT
 // ==/WindhawkMod==
 
 // ==WindhawkModReadme==
 /*
-# Your Awesome Mod
-This is a place for useful information about your mod. Use it to describe the
-mod, explain why it's useful, and add any other relevant details. You can use
-[Markdown](https://en.wikipedia.org/wiki/Markdown) to add links and
-**formatting** to the readme.
+# Audio Output Device Switcher
 
-This short sample customizes Microsoft Paint by forcing it to use just a single
-color, and by blocking file opening. To see the mod in action:
-- Compile the mod with the button on the left or with Ctrl+B.
-- Run Microsoft Paint from the start menu (type "Paint") or by running
-  mspaint.exe.
-- Draw something and notice that the orange color is always used, regardless of
-  the color you pick.
-- Try opening a file and notice that it's blocked.
+Switch between audio output devices by holding **Ctrl** and scrolling the mouse
+wheel over the volume/sound icon in the system tray.
 
-# Getting started
-Check out the documentation
-[here](https://github.com/ramensoftware/windhawk/wiki/Creating-a-new-mod).
+## Usage
+1. Compile the mod with Ctrl+B
+2. Hover over the volume icon in the system tray
+3. Hold Ctrl and scroll up/down to switch audio devices
+4. A popup shows which device is now active
 */
 // ==/WindhawkModReadme==
 
 // ==WindhawkModSettings==
 /*
-# Here you can define settings, in YAML format, that the mod users will be able
-# to configure. Metadata values such as $name and $description are optional.
-# Check out the documentation for more information:
-# https://github.com/ramensoftware/windhawk/wiki/Creating-a-new-mod#settings
-- color:
-  - red: 255
-  - green: 127
-  - blue: 39
-  $name: Custom color
-  $description: This color will be used regardless of the selected color.
-- blockOpen: true
-  $name: Block opening files
-  $description: When enabled, opening files in Paint is not allowed.
+- showNotification: true
+  $name: Show notification
+  $description: Show a popup notification when switching devices
+- notificationDuration: 1500
+  $name: Notification duration (ms)
+  $description: How long to show the notification (in milliseconds)
 */
 // ==/WindhawkModSettings==
 
-// The source code of the mod starts here. This sample was inspired by the great
-// article of Kyle Halladay, X64 Function Hooking by Example:
-// https://kylehalladay.com/blog/2020/11/13/Hooking-By-Example.html
-// If you're new to terms such as code injection and function hooking, the
-// article is great to get started.
+#include <windows.h>
+#include <mmdeviceapi.h>
+#include <endpointvolume.h>
+#include <propsys.h>
+#include <vector>
+#include <string>
 
-#include <gdiplus.h>
+// Define PKEY_Device_FriendlyName manually
+static const PROPERTYKEY PKEY_Device_FriendlyName = {
+    { 0xa45c254e, 0xdf1c, 0x4efd, { 0x80, 0x20, 0x67, 0xd1, 0x46, 0xa8, 0x50, 0xe0 } },
+    14
+};
 
-using namespace Gdiplus;
+// IPolicyConfig interface
+MIDL_INTERFACE("f8679f50-850a-41cf-9c72-430f290290c8")
+IPolicyConfig : public IUnknown
+{
+public:
+    virtual HRESULT STDMETHODCALLTYPE GetMixFormat(PCWSTR, WAVEFORMATEX**) = 0;
+    virtual HRESULT STDMETHODCALLTYPE GetDeviceFormat(PCWSTR, INT, WAVEFORMATEX**) = 0;
+    virtual HRESULT STDMETHODCALLTYPE ResetDeviceFormat(PCWSTR) = 0;
+    virtual HRESULT STDMETHODCALLTYPE SetDeviceFormat(PCWSTR, WAVEFORMATEX*, WAVEFORMATEX*) = 0;
+    virtual HRESULT STDMETHODCALLTYPE GetProcessingPeriod(PCWSTR, INT, PINT64, PINT64) = 0;
+    virtual HRESULT STDMETHODCALLTYPE SetProcessingPeriod(PCWSTR, PINT64) = 0;
+    virtual HRESULT STDMETHODCALLTYPE GetShareMode(PCWSTR, struct DeviceShareMode*) = 0;
+    virtual HRESULT STDMETHODCALLTYPE SetShareMode(PCWSTR, struct DeviceShareMode*) = 0;
+    virtual HRESULT STDMETHODCALLTYPE GetPropertyValue(PCWSTR, const PROPERTYKEY&, PROPVARIANT*) = 0;
+    virtual HRESULT STDMETHODCALLTYPE SetPropertyValue(PCWSTR, const PROPERTYKEY&, PROPVARIANT*) = 0;
+    virtual HRESULT STDMETHODCALLTYPE SetDefaultEndpoint(PCWSTR wszDeviceId, ERole eRole) = 0;
+    virtual HRESULT STDMETHODCALLTYPE SetEndpointVisibility(PCWSTR, INT) = 0;
+};
 
+const CLSID CLSID_CPolicyConfigClient = {
+    0x870af99c, 0x171d, 0x4f9e, {0xaf, 0x0d, 0xe6, 0x3d, 0xf4, 0x0c, 0x2b, 0xc9}
+};
+
+const IID IID_IPolicyConfig = {
+    0xf8679f50, 0x850a, 0x41cf, {0x9c, 0x72, 0x43, 0x0f, 0x29, 0x02, 0x90, 0xc8}
+};
+
+// Settings
 struct {
-    BYTE red;
-    BYTE green;
-    BYTE blue;
-    bool blockOpen;
-} settings;
+    bool showNotification;
+    int notificationDuration;
+} g_settings;
 
-using GdipSetSolidFillColor_t = decltype(&DllExports::GdipSetSolidFillColor);
-GdipSetSolidFillColor_t GdipSetSolidFillColor_Original;
-GpStatus WINAPI GdipSetSolidFillColor_Hook(GpSolidFill* brush, ARGB color) {
-    Wh_Log(L"GdipSetSolidFillColor_Hook: color=%08X", color);
+// Global variables
+HHOOK g_mouseHook = nullptr;
+HWND g_popupWnd = nullptr;
+std::wstring g_popupText;
+const wchar_t* POPUP_CLASS_NAME = L"AudioSwitcherPopup";
 
-    // If the color is not transparent, replace it.
-    if (Color(color).GetAlpha() == 255) {
-        color =
-            Color::MakeARGB(255, settings.red, settings.green, settings.blue);
+struct AudioDevice {
+    std::wstring id;
+    std::wstring name;
+};
+
+std::vector<AudioDevice> GetAudioOutputDevices() {
+    std::vector<AudioDevice> devices;
+    IMMDeviceEnumerator* pEnumerator = nullptr;
+
+    if (FAILED(CoCreateInstance(__uuidof(MMDeviceEnumerator), nullptr, CLSCTX_ALL,
+        __uuidof(IMMDeviceEnumerator), (void**)&pEnumerator))) return devices;
+
+    IMMDeviceCollection* pCollection = nullptr;
+    if (FAILED(pEnumerator->EnumAudioEndpoints(eRender, DEVICE_STATE_ACTIVE, &pCollection))) {
+        pEnumerator->Release();
+        return devices;
     }
 
-    // Call the original function.
-    return GdipSetSolidFillColor_Original(brush, color);
+    UINT count = 0;
+    pCollection->GetCount(&count);
+
+    for (UINT i = 0; i < count; i++) {
+        IMMDevice* pDevice = nullptr;
+        if (SUCCEEDED(pCollection->Item(i, &pDevice))) {
+            LPWSTR deviceId = nullptr;
+            if (SUCCEEDED(pDevice->GetId(&deviceId))) {
+                AudioDevice device;
+                device.id = deviceId;
+                CoTaskMemFree(deviceId);
+
+                IPropertyStore* pProps = nullptr;
+                if (SUCCEEDED(pDevice->OpenPropertyStore(STGM_READ, &pProps))) {
+                    PROPVARIANT varName;
+                    PropVariantInit(&varName);
+                    if (SUCCEEDED(pProps->GetValue(PKEY_Device_FriendlyName, &varName)) && varName.vt == VT_LPWSTR) {
+                        device.name = varName.pwszVal;
+                    } else {
+                        device.name = L"Unknown Device";
+                    }
+                    PropVariantClear(&varName);
+                    pProps->Release();
+                }
+                devices.push_back(device);
+            }
+            pDevice->Release();
+        }
+    }
+
+    pCollection->Release();
+    pEnumerator->Release();
+    return devices;
 }
 
-using GetOpenFileNameW_t = decltype(&GetOpenFileNameW);
-GetOpenFileNameW_t GetOpenFileNameW_Original;
-BOOL WINAPI GetOpenFileNameW_Hook(LPOPENFILENAMEW params) {
-    Wh_Log(L"GetOpenFileNameW_Hook");
+std::wstring GetDefaultDeviceId() {
+    std::wstring deviceId;
+    IMMDeviceEnumerator* pEnumerator = nullptr;
 
-    if (settings.blockOpen) {
-        // Forbid the operation and return without calling the original
-        // function.
-        MessageBoxW(GetActiveWindow(), L"Opening files is forbidden",
-                    L"Surprise!", MB_OK);
-        return FALSE;
+    if (FAILED(CoCreateInstance(__uuidof(MMDeviceEnumerator), nullptr, CLSCTX_ALL,
+        __uuidof(IMMDeviceEnumerator), (void**)&pEnumerator))) return deviceId;
+
+    IMMDevice* pDevice = nullptr;
+    if (SUCCEEDED(pEnumerator->GetDefaultAudioEndpoint(eRender, eConsole, &pDevice))) {
+        LPWSTR id = nullptr;
+        if (SUCCEEDED(pDevice->GetId(&id))) {
+            deviceId = id;
+            CoTaskMemFree(id);
+        }
+        pDevice->Release();
+    }
+    pEnumerator->Release();
+    return deviceId;
+}
+
+bool SetDefaultAudioDevice(const std::wstring& deviceId) {
+    IPolicyConfig* pPolicyConfig = nullptr;
+    HRESULT hr = CoCreateInstance(CLSID_CPolicyConfigClient, nullptr, CLSCTX_ALL,
+        IID_IPolicyConfig, (void**)&pPolicyConfig);
+
+    if (FAILED(hr)) {
+        Wh_Log(L"Failed to create PolicyConfig: 0x%08X", hr);
+        return false;
     }
 
-    return GetOpenFileNameW_Original(params);
+    hr = pPolicyConfig->SetDefaultEndpoint(deviceId.c_str(), eConsole);
+    pPolicyConfig->SetDefaultEndpoint(deviceId.c_str(), eMultimedia);
+    pPolicyConfig->SetDefaultEndpoint(deviceId.c_str(), eCommunications);
+    pPolicyConfig->Release();
+
+    return SUCCEEDED(hr);
+}
+
+LRESULT CALLBACK PopupWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    switch (msg) {
+        case WM_PAINT: {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hwnd, &ps);
+            RECT rc;
+            GetClientRect(hwnd, &rc);
+
+            HBRUSH bgBrush = CreateSolidBrush(RGB(40, 40, 40));
+            FillRect(hdc, &rc, bgBrush);
+            DeleteObject(bgBrush);
+
+            SetTextColor(hdc, RGB(255, 255, 255));
+            SetBkMode(hdc, TRANSPARENT);
+
+            HFONT hFont = CreateFontW(16, 0, 0, 0, FW_NORMAL, FALSE, FALSE, FALSE,
+                DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS,
+                CLEARTYPE_QUALITY, DEFAULT_PITCH | FF_SWISS, L"Segoe UI");
+            HFONT oldFont = (HFONT)SelectObject(hdc, hFont);
+
+            DrawTextW(hdc, g_popupText.c_str(), -1, &rc, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+
+            SelectObject(hdc, oldFont);
+            DeleteObject(hFont);
+            EndPaint(hwnd, &ps);
+            return 0;
+        }
+        case WM_TIMER:
+            KillTimer(hwnd, 1);
+            DestroyWindow(hwnd);
+            g_popupWnd = nullptr;
+            return 0;
+    }
+    return DefWindowProcW(hwnd, msg, wParam, lParam);
+}
+
+void RegisterPopupClass() {
+    static bool registered = false;
+    if (registered) return;
+
+    WNDCLASSEXW wc = {0};
+    wc.cbSize = sizeof(WNDCLASSEXW);
+    wc.lpfnWndProc = PopupWndProc;
+    wc.hInstance = GetModuleHandle(nullptr);
+    wc.lpszClassName = POPUP_CLASS_NAME;
+    wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    RegisterClassExW(&wc);
+    registered = true;
+}
+
+void ShowNotification(const std::wstring& deviceName) {
+    Wh_Log(L"Switched to: %s", deviceName.c_str());
+    if (!g_settings.showNotification) return;
+
+    RegisterPopupClass();
+
+    if (g_popupWnd) {
+        DestroyWindow(g_popupWnd);
+        g_popupWnd = nullptr;
+    }
+
+    g_popupText = L"Audio: " + deviceName;
+
+    int screenWidth = GetSystemMetrics(SM_CXSCREEN);
+    int screenHeight = GetSystemMetrics(SM_CYSCREEN);
+    int popupWidth = 320;
+    int popupHeight = 40;
+    int x = (screenWidth - popupWidth) / 2;
+    int y = screenHeight - 120;
+
+    g_popupWnd = CreateWindowExW(
+        WS_EX_TOPMOST | WS_EX_TOOLWINDOW | WS_EX_LAYERED | WS_EX_NOACTIVATE,
+        POPUP_CLASS_NAME, L"", WS_POPUP,
+        x, y, popupWidth, popupHeight,
+        nullptr, nullptr, GetModuleHandle(nullptr), nullptr
+    );
+
+    if (g_popupWnd) {
+        SetLayeredWindowAttributes(g_popupWnd, 0, 240, LWA_ALPHA);
+        ShowWindow(g_popupWnd, SW_SHOWNOACTIVATE);
+        UpdateWindow(g_popupWnd);
+        SetTimer(g_popupWnd, 1, g_settings.notificationDuration, nullptr);
+    }
+}
+
+void SwitchAudioDevice(int direction) {
+    auto devices = GetAudioOutputDevices();
+    if (devices.size() < 2) {
+        Wh_Log(L"Not enough devices to switch");
+        return;
+    }
+
+    std::wstring currentId = GetDefaultDeviceId();
+    int currentIndex = 0;
+
+    for (size_t i = 0; i < devices.size(); i++) {
+        if (devices[i].id == currentId) {
+            currentIndex = (int)i;
+            break;
+        }
+    }
+
+    int nextIndex = (currentIndex + direction + (int)devices.size()) % (int)devices.size();
+
+    Wh_Log(L"Switching from %d to %d", currentIndex, nextIndex);
+
+    if (SetDefaultAudioDevice(devices[nextIndex].id)) {
+        ShowNotification(devices[nextIndex].name);
+    }
+}
+
+// Check if mouse is over the volume icon area (system tray notification area)
+bool IsOverVolumeIcon(POINT pt) {
+    HWND hTray = FindWindowW(L"Shell_TrayWnd", nullptr);
+    if (!hTray) return false;
+
+    HWND hNotify = FindWindowExW(hTray, nullptr, L"TrayNotifyWnd", nullptr);
+    if (!hNotify) return false;
+
+    // Windows 10/11 system tray area
+    HWND hSysPager = FindWindowExW(hNotify, nullptr, L"SysPager", nullptr);
+    if (hSysPager) {
+        HWND hToolbar = FindWindowExW(hSysPager, nullptr, L"ToolbarWindow32", nullptr);
+        if (hToolbar) {
+            RECT rc;
+            GetWindowRect(hToolbar, &rc);
+            if (PtInRect(&rc, pt)) return true;
+        }
+    }
+
+    // Windows 11 corner icons (where volume icon lives)
+    HWND hCorner = FindWindowExW(hNotify, nullptr, L"Windows.UI.Composition.DesktopWindowContentBridge", nullptr);
+    if (hCorner) {
+        RECT rc;
+        GetWindowRect(hCorner, &rc);
+        if (PtInRect(&rc, pt)) return true;
+    }
+
+    // Main notification area
+    RECT rcNotify;
+    GetWindowRect(hNotify, &rcNotify);
+    if (PtInRect(&rcNotify, pt)) return true;
+
+    return false;
+}
+
+LRESULT CALLBACK MouseProc(int nCode, WPARAM wParam, LPARAM lParam) {
+    if (nCode >= 0 && wParam == WM_MOUSEWHEEL) {
+        MSLLHOOKSTRUCT* pMouse = (MSLLHOOKSTRUCT*)lParam;
+
+        // Check if Ctrl is held and mouse is over volume icon
+        if ((GetAsyncKeyState(VK_CONTROL) & 0x8000) && IsOverVolumeIcon(pMouse->pt)) {
+            short delta = HIWORD(pMouse->mouseData);
+            int direction = (delta > 0) ? -1 : 1;
+
+            Wh_Log(L"Ctrl+Scroll on volume icon, delta=%d", delta);
+            SwitchAudioDevice(direction);
+
+            return 1; // Block the scroll
+        }
+    }
+    return CallNextHookEx(g_mouseHook, nCode, wParam, lParam);
 }
 
 void LoadSettings() {
-    settings.red = Wh_GetIntSetting(L"color.red");
-    settings.green = Wh_GetIntSetting(L"color.green");
-    settings.blue = Wh_GetIntSetting(L"color.blue");
-    settings.blockOpen = Wh_GetIntSetting(L"blockOpen");
+    g_settings.showNotification = Wh_GetIntSetting(L"showNotification");
+    g_settings.notificationDuration = Wh_GetIntSetting(L"notificationDuration");
+    if (g_settings.notificationDuration == 0) g_settings.notificationDuration = 1500;
 }
 
-// The mod is being initialized, load settings, hook functions, and do other
-// initialization stuff if required.
 BOOL Wh_ModInit() {
-    Wh_Log(L"Init");
+    Wh_Log(L"Audio Device Switcher initializing...");
 
+    CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
     LoadSettings();
 
-    HMODULE gdiPlusModule = LoadLibrary(L"gdiplus.dll");
-    GdipSetSolidFillColor_t GdipSetSolidFillColor =
-        (GdipSetSolidFillColor_t)GetProcAddress(gdiPlusModule,
-                                                "GdipSetSolidFillColor");
+    g_mouseHook = SetWindowsHookExW(WH_MOUSE_LL, MouseProc, GetModuleHandle(nullptr), 0);
+    if (!g_mouseHook) {
+        Wh_Log(L"Failed to install mouse hook: %d", GetLastError());
+        return FALSE;
+    }
 
-    Wh_SetFunctionHook((void*)GdipSetSolidFillColor,
-                       (void*)GdipSetSolidFillColor_Hook,
-                       (void**)&GdipSetSolidFillColor_Original);
+    auto devices = GetAudioOutputDevices();
+    Wh_Log(L"Found %d audio devices:", (int)devices.size());
+    for (const auto& dev : devices) {
+        Wh_Log(L"  - %s", dev.name.c_str());
+    }
 
-    Wh_SetFunctionHook((void*)GetOpenFileNameW, (void*)GetOpenFileNameW_Hook,
-                       (void**)&GetOpenFileNameW_Original);
-
+    Wh_Log(L"Ready! Hold Ctrl and scroll over the volume icon to switch devices.");
     return TRUE;
 }
 
-// The mod is being unloaded, free all allocated resources.
 void Wh_ModUninit() {
-    Wh_Log(L"Uninit");
+    Wh_Log(L"Uninitializing...");
+
+    if (g_mouseHook) {
+        UnhookWindowsHookEx(g_mouseHook);
+        g_mouseHook = nullptr;
+    }
+
+    if (g_popupWnd) {
+        DestroyWindow(g_popupWnd);
+        g_popupWnd = nullptr;
+    }
+
+    CoUninitialize();
 }
 
-// The mod setting were changed, reload them.
 void Wh_ModSettingsChanged() {
-    Wh_Log(L"SettingsChanged");
-
+    Wh_Log(L"Settings changed");
     LoadSettings();
 }
